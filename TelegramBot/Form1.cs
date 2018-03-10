f404447167c31f9fb950bcaa96ea04bc2dfd126e
Denis <vip.lichenko@gmail.com>
2018-03-10 17:09:45 +0200

    new cmd

--- TelegramBot/Form1.cs
+++ TelegramBot/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ApiAi;
 
 namespace TelegramBot
 {
@@ -29,6 +30,7 @@ namespace TelegramBot
             string log = "";
             var worker = sender as BackgroundWorker;
             var key = e.Argument as String;
+
             
             // Если ключ не подходит - бросаем исключение 
             try
@@ -44,75 +46,95 @@ namespace TelegramBot
                     var message = update.Message;
                     if (message == null) return;
 
+                    
                     rtbInput.Invoke((MethodInvoker)delegate
                     {
-                        rtbInput.Text += "MSG ID: " + message.MessageId.ToString()
-                        + " | MSG Text: " + message.Text.ToString()
-                        + " | CHAT ID: " + message.From.Id.ToString() + "\n";
+                        try
+                        {
+                            rtbInput.Text += "MSG ID: " + message.MessageId.ToString()
+                            + " | MSG Text: " + message.Text.ToString()
+                            + " | CHAT ID: " + message.From.Id.ToString() + "\n";
+                        }
+                        catch { }
                     });
 
                     if (message.Type == Telegram.Bot.Types.Enums.MessageType.TextMessage)
                     {
+                        var msg = message.Text.ToLower();
                         // Позже, возможно, оптимизирую! :)
-                        if (message.Text.Contains("ello") || message.Text.Contains("Привет") || message.Text.Contains("привет")
-                            || message.Text.Contains("ку") || message.Text.Contains("Ку") || message.Text.Contains("Здарова") || message.Text.Contains("здарова"))
-                        {
-                            int i = rnd.Next(0, Words.hello.Length);
-                            await Bot.SendTextMessageAsync(message.Chat.Id, Words.hello[i], replyToMessageId: message.MessageId);
-                        }
 
-                        else if (message.Text == "/start")
+                        if (msg.Length <= 100)
                         {
-                            // KeyboardButtons
-                            var markup = new Telegram.Bot.Types.ReplyMarkups.ReplyKeyboardMarkup(new[]
+                            if (msg.Contains("привет") || msg.Contains("ку") || msg.Contains("здарова"))
                             {
+                                int i = rnd.Next(0, Words.hello.Length);
+                                await Bot.SendTextMessageAsync(message.Chat.Id, Words.hello[i], replyToMessageId: message.MessageId);
+                            }
+
+                            else if (message.Text == "/start")
+                            {
+                                // KeyboardButtons
+                                var markup = new Telegram.Bot.Types.ReplyMarkups.ReplyKeyboardMarkup(new[]
+                                {
                                     new Telegram.Bot.Types.KeyboardButton("Привет"),
                                     new Telegram.Bot.Types.KeyboardButton("Ку"),
-                                    new Telegram.Bot.Types.KeyboardButton("Hello"),
+                                    new Telegram.Bot.Types.KeyboardButton("Здарова"),
                             });
-                            markup.OneTimeKeyboard = true;
-                            await Bot.SendTextMessageAsync(message.Chat.Id, "Поздоровайтесь! :)", replyMarkup: markup); // Описание действия 
-                        }
+                                markup.OneTimeKeyboard = true;
+                                await Bot.SendTextMessageAsync(message.Chat.Id, "Поздоровайтесь! :)", replyMarkup: markup); // Описание действия 
+                            }
 
-                        else if(message.Text.Contains("идор") || message.Text.Contains("урак") || message.Text.Contains("чмо") || message.Text.Contains("хуй"))
-                        {
-                            await Bot.SendTextMessageAsync(message.Chat.Id, "Я робот, я не могу им быть!", replyToMessageId: message.MessageId);
-                        }
-
-                        else if(message.Text.Contains("зовут"))
-                        {
-                            await Bot.SendTextMessageAsync(message.Chat.Id, "Я хлебушек!");
-                        }
-
-                        else if (message.Text.Contains("дела"))
-                        {
-                            int i = rnd.Next(0, Words.mood.Length);
-                            await Bot.SendTextMessageAsync(message.Chat.Id, Words.mood[i], replyToMessageId: message.MessageId);
-                        }
-
-                        else if (message.Text == "/sendcat")
-                        {
-
-                            int i = rnd.Next(0, Words.cats.Length);
-
-                            Telegram.Bot.Types.FileToSend fl;
-                            Uri cat = new Uri(Words.cats[i]);
-                            fl.Url = cat;
-                            await Bot.SendPhotoAsync(message.Chat.Id, fl, "Мяу!");
-                        }
-                        else if(message.Text == "/smile")
-                        {
-                            await Bot.SendTextMessageAsync(message.Chat.Id, ":smirk:");
-                        }
-                        else
-                        {
-                            //int i = rnd.Next(0, Words.other.Length);
-                            await Bot.SendTextMessageAsync(message.Chat.Id, message.Text, replyToMessageId: message.MessageId);
-                            rtbUndefined.Invoke((MethodInvoker)delegate
+                            else if (msg.Contains("пидор") || msg.Contains("дурак") || msg.Contains("чмо") || msg.Contains("хуй"))
                             {
-                                rtbUndefined.Text += message.Text.ToString() + "\n";
-                            });
+                                await Bot.SendTextMessageAsync(message.Chat.Id, "Я робот, я не могу им быть!", replyToMessageId: message.MessageId);
+                            }
+
+                            else if(msg.Contains("бля"))
+                                await Bot.SendTextMessageAsync(message.Chat.Id, "Сам ты бля!", replyToMessageId: message.MessageId);
+
+                            else if (msg.Contains("зовут"))
+                            {
+                                await Bot.SendTextMessageAsync(message.Chat.Id, "Я хлебушек!");
+                            }
+
+                            else if (msg.Contains("дела"))
+                            {
+                                int i = rnd.Next(0, Words.mood.Length);
+                                await Bot.SendTextMessageAsync(message.Chat.Id, Words.mood[i], replyToMessageId: message.MessageId);
+                            }
+
+                            else if(msg.Contains("олбанский") || msg.Contains("олбанском"))
+                            {
+                                int i = rnd.Next(0, Words.olbanskyi.Length);
+                                await Bot.SendTextMessageAsync(message.Chat.Id, Words.olbanskyi[i], replyToMessageId: message.MessageId);
+                            }
+
+                            else if (message.Text == "/sendcat")
+                            {
+
+                                int i = rnd.Next(0, Words.cats.Length);
+
+                                Telegram.Bot.Types.FileToSend fl;
+                                Uri cat = new Uri(Words.cats[i]);
+                                fl.Url = cat;
+                                await Bot.SendPhotoAsync(message.Chat.Id, fl, "Мяу!");
+                            }
+                            else if (message.Text == "/smile")
+                            {
+                                await Bot.SendTextMessageAsync(message.Chat.Id, ":smirk:");
+                            }
+
+                            else
+                            {
+                                //int i = rnd.Next(0, Words.other.Length);
+                                await Bot.SendTextMessageAsync(message.Chat.Id, message.Text, replyToMessageId: message.MessageId);
+                                rtbUndefined.Invoke((MethodInvoker)delegate
+                                {
+                                    rtbUndefined.Text += message.Text.ToString() + "\n";
+                                });
+                            }
                         }
+                        else await Bot.SendTextMessageAsync(message.Chat.Id, "Многабукаф!!");
                     }
                 };
                 Bot.StartReceiving(); // Запуск приема обновлений 
