3f9b517317375b18e8f9dd627b6f5c86bf07bf79
Denis Lichenko <vip.lichenko@gmail.com>
2019-10-19 19:20:20 +0300

    Init

---
+++ TelegramBot.Bot/Models/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelegramBot.Bot.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; } = "URL/{0}";
+        public static string Name { get; set; } = "";
+        public static string Key { get; set; } = "";
+    }
+}
