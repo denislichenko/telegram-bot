4ca77e9222c310e0ff12880a140fd4ba75aa62e8
Denis Lichenko <vip.lichenko@gmail.com>
2020-03-17 21:08:38 +0200

    remove key

--- TelegramBot.EchoBot/Models/AppSettings.cs
+++ TelegramBot.EchoBot/Models/AppSettings.cs
@@ -6,7 +6,7 @@ namespace TelegramBot.EchoBot.Models
 {
     public static class AppSettings
     {
-        public static string Token { get; set; } = "1124094890:AAHavDxeDNIJg2CzEVYkeMHE4_GMGcBFmiw"; 
+        public static string Token { get; set; } = ""; 
         public static string Name { get; set; }
      }
 }
